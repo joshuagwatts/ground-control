9e9bf25a6c0c20e72ab4206c5e53da1a799bea16
Muse <muse@holowatts.local>
2026-09-17 13:22:57 -0500

    Bump version to 0.2.344

--- www/app.js
+++ www/app.js
@@ -81,7 +81,7 @@ import {
   applyLoadedMapConfig,
   getFlagKindFilter,
   applyFlagKindFilters,
-} from "./wx.js?v=0.2.343";
+} from "./wx.js?v=0.2.344";
 import { pickImageFiles, fileToDataUrl, identifyImage, MAX_CHAT_PHOTOS, cloudVisionReady } from "./vision.js";
 import { SHOTS, identifyShingles, formatVerdict, buildSharePrompt } from "./shingle.js";
 import { shareToChatGpt } from "./share.js";
